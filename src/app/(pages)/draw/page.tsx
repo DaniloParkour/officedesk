274bfbb1bba977d0cc3c5729bf0d93d79d9a0822
Lima de Carvalho <dolh@gft.com>
2023-06-06 17:42:03 -0300

    app draw

--- src/app/(pages)/draw/page.tsx
+++ src/app/(pages)/draw/page.tsx
@@ -11,8 +11,11 @@ interface Point {
 
 export default function Draw() {
 
-  let [canvasWidth, setCanvasWidth] = useState<number>(300);
-  let [canvasHeight, setCanvasHeight] = useState<number>(300);
+  const [color, setColor] = useState<string>("#000000");
+  const [canvasWidth, setCanvasWidth] = useState<number>(300);
+  const [canvasHeight, setCanvasHeight] = useState<number>(300);
+  const [toolMode, setTtoolMode] = useState<"pencil" | "dragDrop">("dragDrop");
+  const [tool, setTool] = useState<"line" | "rect"  | "circle" | "fill_rect"  | "fill_circle" | "erase">("line");
 
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
@@ -20,10 +23,8 @@ export default function Draw() {
   let context2D: CanvasRenderingContext2D | null = null;
   let pointA: Point | null = null;
   let pointB: Point | null = null;
+  let mouseIsPressed = false;
 
-  let tool: "pencil" | "line" | "rect"  | "circle" | "fill_rect"  | "fill_circle" | "erase" = "line";
-  let color: { r:number, g:number, b:number } = {r:0, g:0, b:0};
-  
   useEffect(() => {
     const windowResized = () => {
       setCanvasWidth(window.innerWidth > 640 ? window.innerWidth - 260 : window.innerWidth - 80);
@@ -49,12 +50,10 @@ export default function Draw() {
     if(!context2D) return;
     if(pointA == null || pointB == null) return;
 
-    context2D.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
-    context2D.strokeStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
+    context2D.fillStyle = color;
+    context2D.strokeStyle = color;
 
-    if(tool === "pencil") {
-      //...
-    } else if(tool === "line") {
+    if(tool === "line") {
       context2D.beginPath();
       context2D.moveTo(pointA.x, pointA.y);
       context2D.lineTo(pointB.x, pointB.y);
@@ -78,8 +77,9 @@ export default function Draw() {
     }
     else if(tool === "fill_rect")
       context2D.fillRect(pointA.x, pointA.y, pointB.x - pointA.x, pointB.y - pointA.y);
-
-    //erase
+    else if(tool === "erase")
+      //context2D.clearRect(0, 0, canvas?.width || 100, canvas?.height || 100);
+      context2D.clearRect(pointA.x, pointA.y, pointB.x - pointA.x, pointB.y - pointA.y);
 
     pointA = null;
     pointB = null;
@@ -90,41 +90,67 @@ export default function Draw() {
     getCanvasContex();
     if(!canvas) return;
 
-    const rect = canvas.getBoundingClientRect();
     const x = event.clientX - canvas.offsetLeft;
     const y = event.clientY - canvas.offsetTop;
     pointA = { x, y };
+
+    mouseIsPressed = true;
   }
 
   function mouseUp(event: any) {
     getCanvasContex();
     if(!canvas || pointA == null) return;
 
-    const rect = canvas.getBoundingClientRect();
     const x = event.clientX - canvas.offsetLeft;
     const y = event.clientY - canvas.offsetTop;
     pointB = { x, y };
 
     drawNow();
+
+    mouseIsPressed = false;
+  }
+
+  function mouseMove(event: any) {
+    if(mouseIsPressed && toolMode === "pencil") {
+    
+      getCanvasContex();
+      if(!canvas) return;
+
+      const x = event.clientX - canvas.offsetLeft;
+      const y = event.clientY - canvas.offsetTop;
+
+      if(!pointA) {
+        pointA = { x, y };
+        return;
+      }
+
+      pointB = { x, y };
+
+      drawNow();
+
+      pointA = { x, y };
+    }
   }
   
   return (
     <div className="flex flex-col">
       <div className="bg-cyan-50 w-full flex justify-between px-1">
         <div className="flex gap-2">
-          <IconPencil className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "pencil"} />
-          <IconBackslash className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "line"} />
-          <IconRectangle className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "rect"} />
-          <IconOvalVertical className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "circle"} />
-          <IconRectangleFilled className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "fill_rect"} />
-          <IconOvalVerticalFilled className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "fill_circle"}/>
+          <IconPencil className={`text-cyan-800 hover:text-yellow-500 ${toolMode.toString() === "pencil" && "text-yellow-600"}`} onClick={() => setTtoolMode(toolMode === "pencil" ? "dragDrop" : "pencil")} />
+          <IconBackslash className={`text-cyan-800 hover:text-yellow-500 ${tool.toString() === "line" && "text-yellow-600"}`} onClick={() => setTool("line")} />
+          <IconRectangle className={`text-cyan-800 hover:text-yellow-500 ${tool.toString() === "rect" && "text-yellow-600"}`} onClick={() => setTool("rect")} />
+          <IconOvalVertical className={`text-cyan-800 hover:text-yellow-500 ${tool.toString() === "circle" && "text-yellow-600"}`} onClick={() => setTool("circle")} />
+          <IconRectangleFilled className={`text-cyan-800 hover:text-yellow-500 ${tool.toString() === "fill_rect" && "text-yellow-600"}`} onClick={() => setTool("fill_rect")} />
+          <IconOvalVerticalFilled className={`text-cyan-800 hover:text-yellow-500 ${tool.toString() === "fill_circle" && "text-yellow-600"}`} onClick={() => setTool("fill_circle")}/>
         </div>
         <div className="flex gap-4">
-          <IconEraser className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "erase"} />
-          <IconPalette className="text-cyan-800 hover:text-yellow-500" />
+          <IconEraser className={`text-cyan-800 hover:text-yellow-500 ${tool.toString() === "erase" && "text-yellow-600"}`} onClick={() => setTool("erase")} />
+          <input type="color" id="cor" name="cor" value={color} onChange={(e) => setColor(e.target.value)} className="relative z-0" />
         </div>
       </div>
-      <canvas onMouseDown={mouseDown} onMouseUp={mouseUp} ref={canvasRef} className="bg-white shadow-md"
+      <canvas
+        onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseMove={mouseMove}
+        ref={canvasRef} className="bg-white shadow-md"
         height={canvasHeight}
         width={canvasWidth}
       />
