2550f0de28c3ce4eea9574529b137d23156269bb
Lima de Carvalho <dolh@gft.com>
2023-06-06 11:33:03 -0300

    more draw tools implemented

--- src/app/(pages)/draw/page.tsx
+++ src/app/(pages)/draw/page.tsx
@@ -2,7 +2,7 @@
 
 import { MouseEventHandler, useEffect, useRef, useState } from "react";
 
-import { IconPencil, IconBackslash, IconRectangle, IconOvalVertical, IconRectangleFilled, IconOvalVerticalFilled, IconPalette } from "@tabler/icons-react"
+import { IconPencil, IconBackslash, IconRectangle, IconOvalVertical, IconRectangleFilled, IconOvalVerticalFilled, IconPalette, IconEraser } from "@tabler/icons-react"
 
 interface Point {
   x: number,
@@ -14,14 +14,15 @@ export default function Draw() {
   let [canvasWidth, setCanvasWidth] = useState<number>(300);
   let [canvasHeight, setCanvasHeight] = useState<number>(300);
 
-  const tool: "pencil" | "line" | "rect"  | "circle" | "fill_rect"  | "fill_circle" = "line";
-  
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   let canvas: HTMLCanvasElement | null = null;
   let context2D: CanvasRenderingContext2D | null = null;
   let pointA: Point | null = null;
   let pointB: Point | null = null;
+
+  let tool: "pencil" | "line" | "rect"  | "circle" | "fill_rect"  | "fill_circle" | "erase" = "line";
+  let color: { r:number, g:number, b:number } = {r:0, g:0, b:0};
   
   useEffect(() => {
     const windowResized = () => {
@@ -48,16 +49,37 @@ export default function Draw() {
     if(!context2D) return;
     if(pointA == null || pointB == null) return;
 
-    context2D.fillStyle = "black";
+    context2D.fillStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
+    context2D.strokeStyle = `rgb(${color.r}, ${color.g}, ${color.b})`;
 
-    // context2D.line(pointA.x, pointA.y, pointB.x, pointB.y);
-
-    if(tool === "line") {
+    if(tool === "pencil") {
+      //...
+    } else if(tool === "line") {
       context2D.beginPath();
       context2D.moveTo(pointA.x, pointA.y);
       context2D.lineTo(pointB.x, pointB.y);
       context2D.stroke();
+    } else if(tool === "rect")
+      context2D.strokeRect(pointA.x, pointA.y, pointB.x - pointA.x, pointB.y - pointA.y);
+    else if(tool === "circle" || tool === "fill_circle") {
+      context2D.beginPath();
+      context2D.ellipse(
+        (pointA.x + pointB.x) / 2,
+        (pointA.y + pointB.y) / 2,
+        Math.abs(pointA.x - pointB.x) / 2,
+        Math.abs(pointA.y - pointB.y) / 2,
+        0, 0, 2 * Math.PI
+      );
+
+      if(tool === "fill_circle")
+        context2D.fill();
+      else
+        context2D.stroke();
     }
+    else if(tool === "fill_rect")
+      context2D.fillRect(pointA.x, pointA.y, pointB.x - pointA.x, pointB.y - pointA.y);
+
+    //erase
 
     pointA = null;
     pointB = null;
@@ -90,14 +112,17 @@ export default function Draw() {
     <div className="flex flex-col">
       <div className="bg-cyan-50 w-full flex justify-between px-1">
         <div className="flex gap-2">
-          <IconPencil className="text-cyan-800 hover:text-yellow-500" />
-          <IconBackslash className="text-cyan-800 hover:text-yellow-500" />
-          <IconRectangle className="text-cyan-800 hover:text-yellow-500" />
-          <IconOvalVertical className="text-cyan-800 hover:text-yellow-500" />
-          <IconRectangleFilled className="text-cyan-800 hover:text-yellow-500" />
-          <IconOvalVerticalFilled className="text-cyan-800 hover:text-yellow-500" />
+          <IconPencil className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "pencil"} />
+          <IconBackslash className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "line"} />
+          <IconRectangle className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "rect"} />
+          <IconOvalVertical className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "circle"} />
+          <IconRectangleFilled className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "fill_rect"} />
+          <IconOvalVerticalFilled className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "fill_circle"}/>
         </div>
-        <IconPalette className="text-cyan-800 hover:text-yellow-500" />
+        <div className="flex gap-4">
+          <IconEraser className="text-cyan-800 hover:text-yellow-500" onClick={() => tool = "erase"} />
+          <IconPalette className="text-cyan-800 hover:text-yellow-500" />
+        </div>
       </div>
       <canvas onMouseDown={mouseDown} onMouseUp={mouseUp} ref={canvasRef} className="bg-white shadow-md"
         height={canvasHeight}
